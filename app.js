2367e35df65b2c69798f950dc51de2c7a8b9edd5
sha256 <atharva.kokate@gmail.com>
2020-04-19 13:18:23 +0530

    Add port to app.js

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const commentRoutes = require('./routes/comments');
 const campgroundRoutes = require('./routes/campgrounds');
 const indexRoutes = require('./routes/index'); 
 
+const port = process.env.PORT || 3000;
+
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
@@ -53,6 +55,6 @@ app.use('/',indexRoutes);
 app.use('/campgrounds',campgroundRoutes);
 app.use('/campgrounds/:id/comments',commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, ()=>{
+app.listen(port, ()=>{
     console.log('Yelpcamp server has started...');
 });
